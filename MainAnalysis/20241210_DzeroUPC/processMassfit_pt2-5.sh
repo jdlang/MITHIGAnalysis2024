39d01ab5fdb3dca535b1a5b3f3d5e334d63b0ff8
Jordan Lang <jordandlang.sci@gmail.com>
2025-03-11 15:11:28 +0100

    Automators for microtrees and massfits for pt2-5 only, with configs

---
+++ MainAnalysis/20241210_DzeroUPC/processMassfit_pt2-5.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CONFIG_LIST=(
+#  "pt2-5_fitSettings/fullAnalysis_useGammaNForNgammaForFitFunc.json"
+#  "pt2-5_fitSettings/systDalpha_useGammaNForNgammaForFitFunc.json"
+#  "pt2-5_fitSettings/systDchi2cl_useGammaNForNgammaForFitFunc.json"
+#  "pt2-5_fitSettings/systDsvpv_useGammaNForNgammaForFitFunc.json"
+#  "pt2-5_fitSettings/systDtrkPt_useGammaNForNgammaForFitFunc.json"
+#  "pt2-5_fitSettings/systFitComb_useGammaNForNgammaForFitFunc.json"
+#  "pt2-5_fitSettings/systFitPkBg_useGammaNForNgammaForFitFunc.json"
+#  "pt2-5_fitSettings/systFitSigAlpha_useGammaNForNgammaForFitFunc.json"
+#  "pt2-5_fitSettings/systFitSigMean_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systRapGapLoose_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systRapGapTight_useGammaNForNgammaForFitFunc.json"
+)
+
+if [[ "$1" -eq "1" ]]; then
+    source clean.sh
+    wait
+fi
+
+for CONFIG in ${CONFIG_LIST[@]}; do
+  bash massfit.sh $CONFIG
+  wait
+done
+wait
+
+bash plot.sh "pt2-5_plotSettings/fullAnalysis.json"
+wait
---
+++ MainAnalysis/20241210_DzeroUPC/processSamples_pt2-5.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CONFIG_LIST=(
+  "pt2-5_sampleSettings/fullAnalysis_skimV4.json"
+  "pt2-5_sampleSettings/systDalpha_skimV4.json"
+  "pt2-5_sampleSettings/systDchi2cl_skimV4.json"
+  "pt2-5_sampleSettings/systDsvpv_skimV4.json"
+  "pt2-5_sampleSettings/systDtrkPt_skimV4.json"
+  "pt2-5_sampleSettings/systRapGapLoose_skimV4.json"
+  "pt2-5_sampleSettings/systRapGapTight_skimV4.json"
+)
+
+if [[ "$1" -eq "1" ]]; then
+    source clean.sh
+    wait
+fi
+
+for CONFIG in ${CONFIG_LIST[@]}; do
+    bash makeMicroTree.sh $CONFIG
+    wait
+done
+wait
