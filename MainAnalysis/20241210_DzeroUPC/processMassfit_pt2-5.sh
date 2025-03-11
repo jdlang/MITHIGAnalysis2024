4a650706f7cd61519baba0bad68462314fc2f3ed
Jordan Lang <jordandlang.sci@gmail.com>
2025-03-12 00:12:26 +0100

    Fixed commented out lines

--- MainAnalysis/20241210_DzeroUPC/processMassfit_pt2-5.sh
+++ MainAnalysis/20241210_DzeroUPC/processMassfit_pt2-5.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 CONFIG_LIST=(
-#  "pt2-5_fitSettings/fullAnalysis_useGammaNForNgammaForFitFunc.json"
-#  "pt2-5_fitSettings/systDalpha_useGammaNForNgammaForFitFunc.json"
-#  "pt2-5_fitSettings/systDchi2cl_useGammaNForNgammaForFitFunc.json"
-#  "pt2-5_fitSettings/systDsvpv_useGammaNForNgammaForFitFunc.json"
-#  "pt2-5_fitSettings/systDtrkPt_useGammaNForNgammaForFitFunc.json"
-#  "pt2-5_fitSettings/systFitComb_useGammaNForNgammaForFitFunc.json"
-#  "pt2-5_fitSettings/systFitPkBg_useGammaNForNgammaForFitFunc.json"
-#  "pt2-5_fitSettings/systFitSigAlpha_useGammaNForNgammaForFitFunc.json"
-#  "pt2-5_fitSettings/systFitSigMean_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/fullAnalysis_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systDalpha_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systDchi2cl_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systDsvpv_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systDtrkPt_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systFitComb_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systFitPkBg_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systFitSigAlpha_useGammaNForNgammaForFitFunc.json"
+  "pt2-5_fitSettings/systFitSigMean_useGammaNForNgammaForFitFunc.json"
   "pt2-5_fitSettings/systRapGapLoose_useGammaNForNgammaForFitFunc.json"
   "pt2-5_fitSettings/systRapGapTight_useGammaNForNgammaForFitFunc.json"
 )
