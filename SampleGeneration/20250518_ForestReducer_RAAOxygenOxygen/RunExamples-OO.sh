f169bc3e94d834bad92ca254b67b0a409be4e584
Gian Michele Innocenti <gminnocen@gmail.com>
2025-08-01 20:37:29 -0400

    Add reference plots/files (#179)

--- SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/RunExamples-OO.sh
+++ SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/RunExamples-OO.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source clean.sh
 
-#INPUT=HiForest_2025OO_LowPtCfg_2210.root
-INPUT=/eos/cms/store/group/phys_heavyions/jdlang/Run3_OxygenRAA/PromptForest/IonPhysics0/crab_OO_IonPhysics0_LowPtV2/250711_104114/0002/HiForest_2025OO_LowPtCfg_2210.root
+INPUT=samples/HiForest_2025OO_LowPtCfg_2210.root
+#INPUT=/eos/cms/store/group/phys_heavyions/jdlang/Run3_OxygenRAA/PromptForest/IonPhysics0/crab_OO_IonPhysics0_LowPtV2/250711_104114/0002/HiForest_2025OO_LowPtCfg_2210.root
 OUTPUT=tempOO.root
 
 CORRPATH=${ProjectBase}/CommonCode/root/
@@ -11,7 +11,7 @@ CORRPATH=${ProjectBase}/CommonCode/root/
    --Output $OUTPUT \
    --IsData True \
    --CollisionSystem OO \
-   --Fraction 1.0 \
+   --Fraction 0.1 \
    --ApplyTriggerRejection 0 \
    --ApplyEventRejection false \
    --ApplyTrackRejection false \
