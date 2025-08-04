17fc95b40c2af8c9b218b50e7dfd9862db5dc47d
Gian Michele Innocenti <gminnocen@gmail.com>
2025-08-04 18:06:12 -0400

    Add spectra plotting macro (#186)
    * udpdate code to make it general for OO --> AA
    
    * add Spectra script to plot dNdpT and compare to pp
    
    * add UIC reference files

--- MainAnalysis/20250804_ChargedHadrondNdpt/Spectra.cpp
+++ MainAnalysis/20250804_ChargedHadrondNdpt/Spectra.cpp
@@ -1,20 +1,31 @@
-#include <iostream>
+#include <TCanvas.h>
+#include <TColor.h>
 #include <TFile.h>
 #include <TH1.h>
 #include <TH1F.h>
 #include <TLegend.h>
-#include <TColor.h>
-#include <TCanvas.h>
 #include <TPad.h>
 #include <TStyle.h>
 #include <TSystem.h>
+#include <iostream>
 
-#include "tdrstyle.C"
 #include "CMS_lumi.C"
+#include "tdrstyle.C"
 
-int main() {
+int main(int argc, char *argv[]) {
 
   setTDRStyle();
+
+  if (argc != 8) {
+    std::cout << "Wrong number of arguments!!!!!!!!!!!" << std::endl;
+  }
+  TString label = argv[1];
+  TString filenameNucleusNucleus = argv[2];
+  TString histonameNucleusNucleus = argv[3];
+  TString filenameNucleusNucleusSyst = argv[4];
+  TString histonameNucleusNucleusSyst = argv[5];
+  float lumiscale = atof(argv[6]);
+  TString plotfolder = argv[7];
 
   gStyle->SetOptStat(0);
   gStyle->SetLegendBorderSize(0);
@@ -23,32 +34,24 @@ int main() {
   gStyle->SetPadTickY(1);
   gStyle->SetPadTickX(1);
 
-  //load data
-  TFile * f = TFile::Open("ResultsUIC/pp_OO_raa_20250729_Unblinding_Final_v3.root","read");
-  //get pp and OO data
-  TH1D * pp = (TH1D*)f->Get("pp_Nominal_data_points");
-  TH1D * OO = (TH1D*)f->Get("OO_Nominal_data_points");
+  // load data
+  TFile *fpp = TFile::Open("ResultsUIC/pp_OO_raa_20250729_Unblinding_Final_v3.root", "read");
+  TH1D *pp = (TH1D *)fpp->Get("pp_Nominal_data_points");
+  TH1D *ppSyst = (TH1D *)fpp->Get("pp_Total_uncertainty");
 
-  //load systematics
-  TH1D * ppSyst = (TH1D*)f->Get("pp_Total_uncertainty");
-  TH1D * OOSyst = (TH1D*)f->Get("OO_Total_uncertainty");
+  TFile *fNucleusNucleus = TFile::Open(filenameNucleusNucleus, "read");
+  TH1D *NucleusNucleus = (TH1D *)fNucleusNucleus->Get(histonameNucleusNucleus);
+  TFile *fNucleusNucleusSyst = TFile::Open(filenameNucleusNucleusSyst, "read");
+  TH1D *NucleusNucleusSyst = (TH1D *)fNucleusNucleusSyst->Get(histonameNucleusNucleusSyst);
 
-  //applying lumi scaling for OO
-  OO->Scale(1./1571563.58);
+  // applying lumi scaling for NucleusNucleus
+  NucleusNucleus->Scale(1. / lumiscale);
 
-  //applying a different lumi scaling for low-pt OO
-  for(int i = 0; i<OO->GetNbinsX(); i++){
-    if(OO->GetBinCenter(i)<10){
-      OO->SetBinContent(i,OO->GetBinContent(i)/261927.26*1571563.58);
-      OO->SetBinError(i,  OO->GetBinError(i)/261927.26*1571563.58);
-    }
-  }
-
-  //set up canvas and pads
-  TCanvas * canv2 = new TCanvas("canv2","canv2",700,800);
+  // set up canvas and pads
+  TCanvas *canv2 = new TCanvas("canv2", "canv2", 700, 800);
   canv2->SetBorderSize(0);
-  TPad * pad1 = new TPad("pad1","pad1",0.0,0.25,1.0,1.0,0);
-  TPad * pad2 = new TPad("pad2","pad2",0.0,0.0,1.0,0.25,0);
+  TPad *pad1 = new TPad("pad1", "pad1", 0.0, 0.25, 1.0, 1.0, 0);
+  TPad *pad2 = new TPad("pad2", "pad2", 0.0, 0.0, 1.0, 0.25, 0);
   canv2->SetLineWidth(0);
   pad1->SetBottomMargin(0);
   pad1->SetLeftMargin(0.15);
@@ -64,109 +67,107 @@ int main() {
   pad1->SetLogx();
   pad1->SetLogy();
 
-  //dummy histogram to define the frame
-  TH1D * ppSpecD = new TH1D("specDummy1","",3,2,120);
+  // dummy histogram to define the frame
+  TH1D *ppSpecD = new TH1D("specDummy1", "", 3, 2, 120);
   ppSpecD->GetYaxis()->SetTitle("#frac{1}{4#pi p_{T}} #frac{d#sigma}{dp_{T}} (mb/GeV^{2})");
   ppSpecD->GetYaxis()->SetTitleOffset(1.4);
   ppSpecD->GetYaxis()->SetTitleSize(0.045);
   ppSpecD->GetYaxis()->SetLabelSize(0.04);
   ppSpecD->GetYaxis()->CenterTitle();
   ppSpecD->GetYaxis()->SetLabelOffset(0.002);
-  ppSpecD->GetYaxis()->SetRangeUser(1.1e-14,1e5);
-  ppSpecD->GetXaxis()->SetRangeUser(2,120);
+  ppSpecD->GetYaxis()->SetRangeUser(1.1e-14, 1e5);
+  ppSpecD->GetXaxis()->SetRangeUser(2, 120);
   ppSpecD->Draw();
 
-  //drawing data
+  // drawing data
   pp->SetMarkerStyle(8);
   pp->SetMarkerColor(kBlack);
   pp->SetLineWidth(2);
   pp->SetLineColor(kBlack);
-  pp->GetXaxis()->SetRangeUser(3,100);
+  pp->GetXaxis()->SetRangeUser(3, 100);
   pp->Draw("p same");
 
-  OO->SetMarkerStyle(21);
-  OO->SetLineWidth(2);
-  OO->SetMarkerColor(TColor::GetColor("#5790fc"));
-  OO->SetLineColor(TColor::GetColor("#5790fc"));
-  OO->GetXaxis()->SetRangeUser(3,100);
-  OO->Draw("p same");
+  NucleusNucleus->SetMarkerStyle(21);
+  NucleusNucleus->SetLineWidth(2);
+  NucleusNucleus->SetMarkerColor(TColor::GetColor("#5790fc"));
+  NucleusNucleus->SetLineColor(TColor::GetColor("#5790fc"));
+  NucleusNucleus->GetXaxis()->SetRangeUser(3, 100);
+  NucleusNucleus->Draw("p same");
 
-  //legends
-  TLegend * specLeg = new TLegend(0.6,0.75,1,0.9);
+  // legends
+  TLegend *specLeg = new TLegend(0.6, 0.75, 1, 0.9);
   specLeg->SetTextFont(42);
   specLeg->SetTextSize(0.05);
   specLeg->SetFillStyle(0);
-  specLeg->AddEntry((TObject*)0,"|#eta| < 1",""); 
-  specLeg->AddEntry(pp,"pp","p"); 
-  specLeg->AddEntry(OO,"OO","p");  
+  specLeg->AddEntry((TObject *)0, "|#eta| < 1", "");
+  specLeg->AddEntry(pp, "pp", "p");
+  specLeg->AddEntry(NucleusNucleus, label, "p");
   specLeg->SetFillStyle(0);
-  specLeg->Draw("same"); 
+  specLeg->Draw("same");
 
-  TLegend * systLeg = new TLegend(0.2,0.03,0.6,0.18);
+  TLegend *systLeg = new TLegend(0.2, 0.03, 0.6, 0.18);
   systLeg->SetTextFont(42);
   systLeg->SetTextSize(0.05);
   systLeg->SetFillStyle(0);
-  systLeg->AddEntry((TObject*)0,"","");
-  systLeg->AddEntry(ppSyst,"pp","f");
-  systLeg->AddEntry(OOSyst,"OO","f");
+  systLeg->AddEntry((TObject *)0, "", "");
+  systLeg->AddEntry(ppSyst, "pp", "f");
+  systLeg->AddEntry(NucleusNucleusSyst, label, "f");
   systLeg->SetFillStyle(0);
   systLeg->Draw("same");
 
-  TLegend * systLeg2 = new TLegend(0.15,0.03,0.65,0.23);
+  TLegend *systLeg2 = new TLegend(0.15, 0.03, 0.65, 0.23);
   systLeg2->SetTextFont(42);
   systLeg2->SetTextSize(0.05);
   systLeg2->SetFillStyle(0);
   systLeg2->SetTextAlign(22);
-  systLeg2->AddEntry((TObject*)0,"Normalization","");
-  systLeg2->AddEntry((TObject*)0,"uncertainty","");
-  systLeg2->AddEntry((TObject*)0,"5%","");
-  systLeg2->AddEntry((TObject*)0,"5%","");
+  systLeg2->AddEntry((TObject *)0, "Normalization", "");
+  systLeg2->AddEntry((TObject *)0, "uncertainty", "");
+  systLeg2->AddEntry((TObject *)0, "5%", "");
+  systLeg2->AddEntry((TObject *)0, "5%", "");
   systLeg2->SetFillStyle(0);
   systLeg2->Draw("same");
 
-  //lower panel
+  // lower panel
   pad2->cd();
   pad2->SetLogx();
-  TH1D * ppSpecD2 = new TH1D("specDummy2","",3,2,120);
-  ppSpecD2->GetYaxis()->SetRangeUser(0.0,9.999);
-  ppSpecD2->GetYaxis()->SetNdivisions(4,4,0,kTRUE);
+  TH1D *ppSpecD2 = new TH1D("specDummy2", "", 3, 2, 120);
+  ppSpecD2->GetYaxis()->SetRangeUser(0.0, 9.999);
+  ppSpecD2->GetYaxis()->SetNdivisions(4, 4, 0, kTRUE);
   ppSpecD2->GetYaxis()->SetTitleOffset(0.4);
   ppSpecD2->GetYaxis()->SetTitleFont(42);
-  ppSpecD2->GetYaxis()->SetTitleSize(0.095*1.2);
-  ppSpecD2->GetYaxis()->SetLabelSize(0.095*1.2);
+  ppSpecD2->GetYaxis()->SetTitleSize(0.095 * 1.2);
+  ppSpecD2->GetYaxis()->SetLabelSize(0.095 * 1.2);
   ppSpecD2->GetXaxis()->SetTitleFont(42);
-  ppSpecD2->GetYaxis()->SetTitle(Form("Syst. uncert. (%s)","%"));
-  ppSpecD2->GetXaxis()->SetRangeUser(2,120);
+  ppSpecD2->GetYaxis()->SetTitle(Form("Syst. uncert. (%s)", "%"));
+  ppSpecD2->GetXaxis()->SetRangeUser(2, 120);
   ppSpecD2->GetXaxis()->SetTitle("p_{T} (GeV)");
-  ppSpecD2->GetXaxis()->SetTitleSize(0.1*1.2);
-  ppSpecD2->GetXaxis()->SetLabelSize(0.1*1.2);
+  ppSpecD2->GetXaxis()->SetTitleSize(0.1 * 1.2);
+  ppSpecD2->GetXaxis()->SetLabelSize(0.1 * 1.2);
   ppSpecD2->GetXaxis()->SetTitleOffset(1.2);
   ppSpecD2->GetXaxis()->CenterTitle();
   ppSpecD2->GetXaxis()->SetTickLength(0.06);
   ppSpecD2->Draw();
 
-  //drawing systematics
+  // drawing systematics
   ppSyst->SetFillColor(kBlack);
   ppSyst->SetFillStyle(3003);
-  ppSyst->GetXaxis()->SetRangeUser(3,100);
+  ppSyst->GetXaxis()->SetRangeUser(3, 100);
   ppSyst->SetLineColor(kBlack);
   ppSyst->SetLineWidth(3);
   ppSyst->Draw("same HIST");
 
-  //drawing systematics
-  OOSyst->SetFillColor(TColor::GetColor("#5790fc"));
-  OOSyst->SetFillStyle(3004);
-  OOSyst->GetXaxis()->SetRangeUser(3,100);
-  OOSyst->SetLineColor(TColor::GetColor("#5790fc"));
-  OOSyst->SetLineWidth(3);
-  OOSyst->Draw("same HIST");
+  // drawing systematics
+  NucleusNucleusSyst->SetFillColor(TColor::GetColor("#5790fc"));
+  NucleusNucleusSyst->SetFillStyle(3004);
+  NucleusNucleusSyst->GetXaxis()->SetRangeUser(3, 100);
+  NucleusNucleusSyst->SetLineColor(TColor::GetColor("#5790fc"));
+  NucleusNucleusSyst->SetLineWidth(3);
+  NucleusNucleusSyst->Draw("same HIST");
 
-  CMS_lumi( pad1, 0,11);
+  CMS_lumi(pad1, 0, 11);
 
-  canv2->SaveAs("plots/Figure_001.pdf");
-  canv2->SaveAs("plots/Figure_001.png");
-  canv2->SaveAs("plots/Figure_001.C");
-
+  canv2->SaveAs(Form("%s/Figure_001.pdf", plotfolder.Data()));
+  canv2->SaveAs(Form("%s/Figure_001.png", plotfolder.Data()));
+  canv2->SaveAs(Form("%s/Figure_001.C", plotfolder.Data()));
   return 0;
 }
-
--- MainAnalysis/20250804_ChargedHadrondNdpt/doAnalysisAllOO.sh
+++ MainAnalysis/20250804_ChargedHadrondNdpt/doAnalysisAllOO.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
-LUMIAAtoPP=1.0
-
+LUMIAAtoPP=150000
 
 MODE="output_OOReferenceCentralValue"
 ./RunParallelReadParam.sh --Output ${MODE} --Input OO_InputFileList_Reference.txt --CollisionSystem true --ApplyEventSelection true --UseEventWeight false --UseSpeciesWeight false --UseTrackWeight true --TrackWeightSelection 2 --MinTrackPt 400 --MinLeadingTrackPt -1 --TriggerChoice 1 --EventSelectionOption 1 --SpeciesCorrectionOption 0 --ScaleFactor 1
@@ -12,6 +10,17 @@ root -l -b -q 'PlotTrkPtVariantsWithRatioToUnweighted_Save.C("'"${MODE}/MergedOu
 
 echo "Running spectra macro..."
 g++ -o Spectra Spectra.cpp $(root-config --cflags --glibs) -lASImage
-./Spectra
+
+# Run the spectra macro for OO
+# Inputs:
+# 1. Collision system: e.g. "OO"
+# 2. Input file for AA spectra
+# 3. Histogram name for AA spectra
+# 4. Input file for AA systematics
+# 5. Output histogram name for AA systematics
+# 6. Lumi scaling for OO
+# 7. Output directory for results
+
+./Spectra "OO" "output_OOReferenceCentralValue/MergedOutput.root" "hTrkPt" "ResultsUIC/pp_OO_raa_20250729_Unblinding_Final_v3.root" "OO_Total_uncertainty" $LUMIAAtoPP "output_OOReferenceCentralValue"
 
 
